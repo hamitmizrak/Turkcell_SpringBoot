230925f47acc37fe392d196a0f5d2a7dc3c7cd55
hamitmizrak <hamitmizrak@gmail.com>
2022-08-01 11:27:54 +0300

    log

--- src/main/java/com/hamitmizrak/ui/mvc/ProductMvcEntityController.java
+++ src/main/java/com/hamitmizrak/ui/mvc/ProductMvcEntityController.java
@@ -2,13 +2,14 @@ package com.hamitmizrak.ui.mvc;
 
 import java.util.UUID;
 
-import com.hamitmizrak.data.entity.ProductEntity;
-import com.hamitmizrak.data.repository.IProductRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hamitmizrak.data.entity.ProductEntity;
+import com.hamitmizrak.data.repository.IProductRepository;
 
 @Controller
 public class ProductMvcEntityController {
@@ -18,12 +19,13 @@ public class ProductMvcEntityController {
 	
 	// http://localhost:8080/product/entity
 	@GetMapping("/product/entity")
+	@ResponseBody
 	public String mvcEntityMethod(Model model) {
 		UUID uuid = UUID.randomUUID();
 		ProductEntity entity = new ProductEntity("ürün adı44", uuid.toString());
 		model.addAttribute("entity_key", entity);
 		repository.save(entity);
-		return "entity_mvc";
+		return "Product Eklend";
 	}
 	
 }
