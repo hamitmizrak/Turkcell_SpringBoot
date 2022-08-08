318478691988f02b84319399d96995aebbe1242d
hamitmizrak <hamitmizrak@gmail.com>
2022-08-08 10:23:05 +0300

    rest xml

--- src/main/java/com/hamitmizrak/ui/rest/impl/AdminRestController.java
+++ src/main/java/com/hamitmizrak/ui/rest/impl/AdminRestController.java
@@ -23,6 +23,8 @@ public class AdminRestController {
 				+ "  \"java\":{\r\n" + "    \"name\":\"javase\",\r\n" + "    \"version\":\"18\"\r\n" + "  }\r\n" + "}";
 	}
 	
+	// 1.SEÇENEK: jackson dependency eklendiğinde default olan json yerine XML
+	// gelir.
 	// http://localhost:8080/api/v1/rest/object2
 	@GetMapping(value = "rest/object2")
 	public AdminDto getRest2() {
@@ -30,10 +32,21 @@ public class AdminRestController {
 		return adminDto;
 	}
 	
+	// 2.SEÇENEK : jackson dependency eklendiğinde zaten XML geliyor ancak biz
+	// işimiz garantiye alıyoruz
 	// http://localhost:8080/api/v1/rest/object3
 	// import org.springframework.http.MediaType;
 	@GetMapping(value = "rest/object3", produces = MediaType.APPLICATION_JSON_VALUE)
 	public AdminDto getRest3() {
+		AdminDto adminDto = AdminDto.builder().adminId(0L).adminName("admin adı").adminSurname("admin soyadı").build();
+		return adminDto;
+	}
+	
+	// 3.SEÇENEK (Türkçe karakteri çözmek) ==> UTF-8
+	// http://localhost:8080/api/v1/rest/object4
+	// import org.springframework.http.MediaType;
+	@GetMapping(value = "rest/object4", produces = "application/json;charset=UTF-8")
+	public AdminDto getRest4() {
 		AdminDto adminDto = AdminDto.builder().adminId(0L).adminName("admin adı").adminSurname("admin soyadı").build();
 		return adminDto;
 	}
