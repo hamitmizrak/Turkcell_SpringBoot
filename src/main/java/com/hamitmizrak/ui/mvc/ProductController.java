2145144a86f886481e83f1aa92ba6f6a2d09a3e7
hamitmizrak <hamitmizrak@gmail.com>
2022-08-09 09:49:40 +0300

    RestTemplate ProductDto

--- src/main/java/com/hamitmizrak/ui/mvc/ProductController.java
+++ src/main/java/com/hamitmizrak/ui/mvc/ProductController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.hamitmizrak.business.dto.ProductDto;
+
 @Controller
 public class ProductController {
 	
 	// http://localhost:8080/client/controller/string
+	// SERVER
 	@GetMapping("client/controller/string")
 	@ResponseBody
 	public String getClientData() {
@@ -16,6 +19,17 @@ public class ProductController {
 		RestTemplate restTemplate = new RestTemplate();
 		String jsonData = restTemplate.getForObject(URL, String.class);
 		return jsonData;
+	}
+	
+	// http://localhost:8080/client/controller/productdto
+	// SERVER
+	@GetMapping("client/controller/productdto")
+	@ResponseBody
+	public ProductDto getClientObjectData() {
+		final String URL = "http://localhost:8080/server/v1/object";
+		RestTemplate restTemplate = new RestTemplate();
+		ProductDto productDto = restTemplate.getForObject(URL, ProductDto.class);
+		return productDto;
 	}
 	
 }
--- src/main/java/com/hamitmizrak/ui/rest/impl/ProductRestController.java
+++ src/main/java/com/hamitmizrak/ui/rest/impl/ProductRestController.java
@@ -4,14 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hamitmizrak.business.dto.ProductDto;
+
 @RestController
 @RequestMapping("/server/v1")
 public class ProductRestController {
 	
 	// http://localhost:8080/server/v1/manueljson
+	// SERVER
 	@GetMapping("manueljson")
 	public String sendServerData() {
 		return "{\r\n" + "  \"adi\":\"Hamit\",\r\n" + "  \"soyadi\":\"Mızrak\"\r\n" + "}";
+	}
+	
+	// http://localhost:8080/server/v1/object
+	// SERVER
+	@GetMapping("object")
+	public ProductDto sendServerObjectData() {
+		ProductDto productDto = ProductDto.builder().productId(1L).productName("ürün adı").productCode("ürün kodu")
+				.productPrice(44).build();
+		return productDto;
 	}
 	
 }
