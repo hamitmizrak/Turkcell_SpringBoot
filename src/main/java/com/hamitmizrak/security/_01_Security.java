7999cace3bf0b90030fe2ce43b2f6280ac945721
hamitmizrak <hamitmizrak@gmail.com>
2022-08-11 10:08:55 +0300

    websecurity

--- src/main/java/com/hamitmizrak/security/_01_Security.java
+++ src/main/java/com/hamitmizrak/security/_01_Security.java
@@ -1,8 +1,18 @@
 package com.hamitmizrak.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// Yukarıdaki kod aslında @SpringBootApplication'da yazdığımız Exclude ile
+// aynıdır'
+// WebSecurity kapatmak
+// @SpringBootApplication(exclude = {
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+// })
 
 // org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 // Bu ise WebSecurity generated code çalışır ancak WebSecrity login'e redirect
@@ -15,16 +25,20 @@ public class _01_Security extends WebSecurityConfigurerAdapter {
 		// httpBasic üzerinden sisteme giriş
 		// http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 		
-		// formRegister
+		// formRegister üzerinden sisteme giriş
 		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
 	}
 	
+	// Bu metotu ben yazdım ==> myUserPasswordRoles
+	// bu metotudun görevi yeni kullanıcı eklemek ve o kullanıcıya rol vermek
+	@Autowired
+	public void myUserPasswordRoles(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		// inMemoryAuthentication=Database üzerinden değil Spring içinde bu veriler
+		// //NoEncrpted: yani Maskesiz saklanacak noop:Spring üzerinden maskelenmeden
+		// sakalama yarar.
+		authenticationManagerBuilder.inMemoryAuthentication().withUser("hamitmizrak").password("{noop}root")
+				.roles("ADMIN");
+		
+	}
+	
 }
-
-// Yukarıdaki kod aslında @SpringBootApplication'da yazdığımız Exclude ile
-// aynıdır'
-// WebSecurity kapatmak
-// @SpringBootApplication(exclude = {
-// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-// })
